7d493e812f7332ad859647754edb2799c5c3764e
Aayushgit18 <aayushchoundiye@gmail.com>
2026-01-05 11:55:10 +0530

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const os = require('os');
+
 const app = express();
 
-const NODE_NUMBER = process.env.NODE_NUMBER || "01";
+const PORT = process.env.PORT || 3000;
+const NODE_NUMBER = process.env.NODE_NUMBER || "00";
 const VERSION = process.env.APP_VERSION || "1.0";
+
 const HOSTNAME = `${os.hostname()}-Node-${NODE_NUMBER}`;
 
 app.get('/', (req, res) => {
-  res.send(`<h1>${HOSTNAME}</h1><h3>Version ${VERSION}</h3>`);
+  res.send(`
+    <h1>${HOSTNAME}</h1>
+    <h3>Version ${VERSION}</h3>
+  `);
 });
 
-app.listen(3000, () => console.log("App running"));
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});
