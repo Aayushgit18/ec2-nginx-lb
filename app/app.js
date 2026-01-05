14cb819ac4c15e78c493119863933220d0f7b8e2
Aayushgit18 <aayushchoundiye@gmail.com>
2026-01-05 13:01:21 +0530

    Update app version to 0.0 and improve HTML structure

--- app/app.js
+++ app/app.js
@@ -5,14 +5,21 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 const NODE_NUMBER = process.env.NODE_NUMBER || "00";
-const VERSION = process.env.APP_VERSION || "1.0";
+const VERSION = process.env.APP_VERSION || "0.0";
 
 const HOSTNAME = `${os.hostname()}-Node-${NODE_NUMBER}`;
 
 app.get('/', (req, res) => {
   res.send(`
-    <h1>${HOSTNAME}</h1>
-    <h3>Version ${VERSION}</h3>
+    <html>
+      <head>
+        <title>Node App</title>
+      </head>
+      <body style="text-align:center;font-family:Arial">
+        <h1>${HOSTNAME}</h1>
+        <h3>Version ${VERSION}</h3>
+      </body>
+    </html>
   `);
 });
 
