f87e2f60be383d3ee2a471c28d7e4e5b4de152e9
Aayushgit18 <aayushchoundiye@gmail.com>
2026-01-06 12:19:16 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,7 @@
 variable "key_name" {
-  description = "AWS key pair name"
-  type        = string
+  description = "AWS EC2 key pair name"
+}
+
+variable "private_key_path" {
+  description = "Path to EC2 private key"
 }
